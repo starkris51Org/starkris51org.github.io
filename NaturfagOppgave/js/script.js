a2d53cd100dfc0dedc3e1edd150725ffbfd93e53
starkris51 <75519603+starkris51@users.noreply.github.com>
2022-12-05 09:16:26 +0100

    added files

---
+++ NaturfagOppgave/js/script.js
@@ -0,0 +1,17 @@
+function ReturnHome() {
+    window.location.href = ("/index.html")
+}
+
+function SunnKostholdPage() {
+    window.location.href = "/html/sunnKosthold.html";
+}
+
+function GodHelsePage() {
+    window.location.href = ("/html/godHelse.html");
+}
+
+/* This got never used due to the lack of time
+function Meny() {
+
+}
+*/
